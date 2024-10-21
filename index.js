f0164ad509f4b7b798c57c3c422fa59b5bb65e9d
logeshloki585 <83359813+logeshloki585@users.noreply.github.com>
2024-10-21 16:49:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 connectDB();
 
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000','http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
