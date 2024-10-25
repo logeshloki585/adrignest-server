25ee6b9548678e9b0e69aa564817bd9de2e1138c
prashant kumar <80661803+prashant67690@users.noreply.github.com>
2024-10-25 14:52:16 +0530

    removed the slash

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:5173",
-      "https://adrig-nest.vercel.app/",
+      "https://adrig-nest.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
